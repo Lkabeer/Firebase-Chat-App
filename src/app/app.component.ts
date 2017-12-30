ac581e6ecc725e141b031d975e37dd71ff560c8b
L kabeer <ahmed@Lkabeer.com>
2017-12-31 00:56:58 +0200

    v1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, AngularFireDatabaseModule  } from "angularfire2/database";
+import { AngularFireAuth } from "angularfire2/auth";
+import { Observable } from "rxjs/Observable";
+import * as firebase from "firebase/app";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  
+  items: FirebaseListObservable<any[]>
+  msgVal: string = '';
+
+  constructor(public afAuth: AngularFireAuth, public afDatabase: AngularFireDatabase) {
+    this.items = afDatabase.list('/messages', {
+      query: {
+        limitToLast: 5
+      }
+    });
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  chatSend(msg: string) {
+    this.items.push({
+      message: msg
+    });
+    this.msgVal = '';
+  }
 }

ffa8c60111aa2d7159a821def69844324337e731
L kabeer <ahmed@Lkabeer.com>
2017-12-30 22:56:57 +0200

    v1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyADB91lDP2Hv4xeMjq9aJ_-92fg88w7BOc",
+  authDomain: "angularfirebasechatapp.firebaseapp.com",
+  databaseURL: "https://angularfirebasechatapp.firebaseio.com",
+  projectId: "angularfirebasechatapp",
+  storageBucket: "angularfirebasechatapp.appspot.com",
+  messagingSenderId: "123136448958"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +24,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
